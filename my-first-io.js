ad093b6028137b607d64e8b14aa6989208d36690
Rootul <rootulp@gmail.com>
2014-09-14 21:05:22 -0400

    complete my-first-io

--- my-first-io.js
+++ my-first-io.js
@@ -1,3 +1,7 @@
 var fs = require('fs')
 
-fs.readFileSync('/path/to/file')
+var pathToFile = process.argv[2]
+var buf = fs.readFileSync(pathToFile)
+var str = buf.toString()
+var arr = str.split('\n')
+console.log(arr.length - 1)
