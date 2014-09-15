786c5493e009a3cd4cba64b4a8cd9ab0011508af
Rootul <rootulp@gmail.com>
2014-09-14 21:19:28 -0400

    clean up filtered ls

--- filtered-ls.js
+++ filtered-ls.js
@@ -2,11 +2,11 @@ var fs = require('fs')
 var path = require('path')
 
 var pathToDir = process.argv[2]
-var extension = process.argv[3]
+var extension = "." + process.argv[3]
 fs.readdir(pathToDir, function callback (err, list) { 
     if (err) throw err;
     for (var i =0; i<list.length; i++){
-        if (path.extname(list[i]) === "." + extension) {
+        if (path.extname(list[i]) === extension) {
             console.log(list[i])
         }
     }
