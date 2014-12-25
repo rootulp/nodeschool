f2965d75d305b01554a41fa5eb36ed65469f9d32
Rootul Patel <rootulp@gmail.com>
2014-12-24 19:28:17 -0500

    json meee

---
+++ expressworks/json-me.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+
+app.get('/books', function(req, res) {
+    fs.readFile(process.argv[3], function (e, data) {
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(process.argv[2]);
