0a3030f891f08271f99cef29e1831e528bf95eec
Rootul Patel <rootulp@gmail.com>
2015-03-21 20:31:25 -0400

    solve higher order functions with recursion

---
+++ functional_javascript/higher-order-functions.js
@@ -0,0 +1,11 @@
+function repeat(operation, num) {
+  if (num === 0){
+    return;
+  } else {
+    operation();
+    return repeat(operation, num-1);
+  }
+}
+
+// Do not remove the line below
+module.exports = repeat
