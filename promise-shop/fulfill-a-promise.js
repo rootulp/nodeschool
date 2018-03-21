e23053cb8d66fef585f74a28639ad2d6407c4cdb
Rootul Patel <rootulp@palantir.com>
2018-03-21 19:51:49 +0000

    Solve fulfill-a-promise

---
+++ promise-shop/fulfill-a-promise.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(() => {fulfill('FULFILLED!')}, 300);
+});
+
+promise.then(contents => {console.log(contents)})
