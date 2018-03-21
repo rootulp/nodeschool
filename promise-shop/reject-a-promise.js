79ab81f55cea6eb408099eb02643e2e160dc5cb4
Rootul Patel <rootulp@palantir.com>
2018-03-21 19:56:22 +0000

    Solve reject a promise

---
+++ promise-shop/reject-a-promise.js
@@ -0,0 +1,9 @@
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(() => {reject(new Error('REJECTED!'))}, 300)
+});
+
+function onReject (error) {
+    console.log(error.message)
+}
+
+promise.then(() => {}, onReject)
